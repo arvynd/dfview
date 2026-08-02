b0884dd9b28b08794524c97a5ecd1cc53c0cb2bc
Aravind <aravindpramod@outlook.com>
2026-08-02 21:49:58 +0530

    Visidata hooked

--- lua/df-nvim.lua
+++ lua/df-nvim.lua
@@ -1,17 +1,17 @@
 local M = {}
 
-function M.say_hello()
-	print("Hello from neovim")
+function M.dflens()
+	vim.cmd("vsplit|terminal vd /tmp/dflens_test.csv")
 end
 
 function M.setup(opts)
 	opts = opts or {}
 
-	vim.api.nvim_create_user_command("HelloWorld", M.say_hello, {})
+	vim.api.nvim_create_user_command("OpenDFLens", M.dflens, {})
 
 	local keymap = opts.keymap or "<leader>hw"
 
-	vim.keymap.set("n", keymap, M.say_hello, {
+	vim.keymap.set("n", keymap, M.dflens, {
 		desc = "From Plugin",
 		silent = true,
 	})
